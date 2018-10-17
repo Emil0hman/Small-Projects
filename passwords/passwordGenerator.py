50119c21d99eccd8ddce4c82e18dff501107ce64
Emil Öhman <41198263+Emil0hman@users.noreply.github.com>
2018-10-17 19:37:22 +0200

    Added adjustable length of password and made arguments non-mandatory

--- passwords/passwordGenerator.py
+++ passwords/passwordGenerator.py
@@ -1,18 +1,18 @@
-def genNPass(n):
+def genNPass(n=1, l=[8, 14]):
     import random
 
     letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    chars = ['@','#','/','=','?', '%', '!', '&', '.']
+    chars = ['@','#','/','=','?', '%', '!', '&', '.', '_', '-']
 
     password = []
 
     for q in range(n):
         temp = ''
-        for i in range(random.randint(8, 15)):
+        for i in range(random.randint(l[0], l[1])):
             if i % random.randint(2, 8) == 0 and not i == 0:
-                temp = temp + chars[random.randint(0, 7)]
+                temp = temp + chars[random.randint(0, len(chars) - 1)]
             else:
-                temp = temp + letters[random.randint(0, 51)]
+                temp = temp + letters[random.randint(0, len(letters) - 1)]
         password.append(temp)
 
     return password
